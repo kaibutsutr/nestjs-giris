0bdca6c02cb1fba81ef71deb0886963aa2a4660e
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-11-05 00:28:39 +0300

    routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
 // import these from common
-@Controller()
+@Controller("/api")
 export class AppController {
   // export this block
-  @Get()
-  getRootRoute() {
+  @Get("/") getRootRoute() {
+    return "Main page";
+  }
+  @Get("/hello") //hello route
+  getHelloRoute() {
     return "Hello world";
+  }
+  @Get("/login") // login route
+  getLoginRoute() {
+    return "Login page";
   }
 }
